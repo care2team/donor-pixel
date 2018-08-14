dfb78173fb7053fa96862c7d5d26127df57960d3
martin <martin@care2team.com>
2018-08-14 16:12:53 -0700

    [master] install the sha256 hasher library

--- src/pixel.js
+++ src/pixel.js
@@ -1,3 +1,36 @@
-// Content goes here
+var sha256 = require('js-sha256').sha256;
 
-$a = "test";
+
+(function () {
+
+    var c2td = care2TrackDonation;
+    var queue = care2TrackDonation.queue || [];
+
+    var callMethod = c2td.callMethod = function (args) {
+        var clientid = args[0];
+        var email = args[1];
+        var value = args[2];
+
+        var url = 'https://www.care2.com/tr'
+            + '?clientid=' + clientid
+            + '&emailhash=' + sha256(email)
+            + '&value=' + value;
+
+
+        var img = document.createElement('img');
+        img.height = '1';
+        img.width = '1';
+        img.alt = '';
+        img.style = 'display:none';
+        img.src = url;
+
+
+        window.document.body.appendChild(img);
+    };
+
+    for (var i = 0; i < queue.length; i++) {
+        callMethod(queue[i]);
+    }
+
+})();
+
